9c84d2553d17bd40b20793bd79ee28b033a6aa72
khaoulamejri <44674378+khaoulamejri@users.noreply.github.com>
2020-06-09 20:07:01 +0100

    athenticationwithjwt+jsf
    login+register+apimail+spring security

---
+++ gestionjardin/src/main/java/tn/esprit/spring/entity/OnRegistrationCompleteEvent.java
@@ -0,0 +1,64 @@
+package tn.esprit.spring.entity;
+
+import java.util.Locale;
+
+import org.springframework.context.ApplicationEvent;
+
+public class OnRegistrationCompleteEvent extends ApplicationEvent {
+    private String appUrl;
+    private Locale locale;
+    private User user;
+ 
+    public OnRegistrationCompleteEvent(
+      User user, Locale locale, String appUrl) {
+        super(user);
+         
+        this.user = user;
+        this.locale = locale;
+        this.appUrl = appUrl;
+    }
+
+	public String getAppUrl() {
+		return appUrl;
+	}
+
+	public OnRegistrationCompleteEvent(String appUrl, User user) {
+		super(user);
+		this.appUrl = appUrl;
+		this.user = user;
+	}
+
+
+	public OnRegistrationCompleteEvent(Object source, String appUrl) {
+		super(source);
+		this.appUrl = appUrl;
+	}
+
+	public OnRegistrationCompleteEvent(Object source, String appUrl, User user) {
+		super(source);
+		this.appUrl = appUrl;
+		this.user = user;
+	}
+
+	public void setAppUrl(String appUrl) {
+		this.appUrl = appUrl;
+	}
+
+	public Locale getLocale() {
+		return locale;
+	}
+
+	public void setLocale(Locale locale) {
+		this.locale = locale;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+     
+    
+}
